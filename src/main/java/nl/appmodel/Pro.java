74388a29b60c85f03618f9de0d23565fb2db5a6a
Michael Bakker <michael.bakker1986@gmail.com>
2021-02-06 22:08:00 +0100

    Initial commit iframe-embed (known bug: width/height, youporn verification) Tooltip Use of small IDS in react Improve logging

--- src/main/java/nl/appmodel/Pro.java
+++ src/main/java/nl/appmodel/Pro.java
@@ -1,40 +1,20 @@
 package nl.appmodel;
 
-/*import lombok.Data;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Include;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;*/
+import lombok.extern.slf4j.Slf4j;
 import javax.persistence.*;
-import java.util.Objects;
 @Entity
 @Table(name = "pro", schema = "prosite")
-/*@Slf4j
+@Slf4j
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@NoArgsConstructor*/
+@NoArgsConstructor
 public class Pro {
-    @Id @GeneratedValue
-    /*@Include*/
-            long id;
-    @Column                   String  thumbs;
-    @Column                   byte[]  ref;
-    @Column(nullable = false) boolean downloaded = true;
-    public String getSuffix() {
-        return ".jpg";
-    }
-    public String getFileName() {
-        var s        = thumbs.split("[,;]")[0];
-        var filename = s.substring(s.lastIndexOf("/") + 1);
-        return filename.substring(0, filename.length() - 4);
-    }
-    @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pro pro = (Pro) o;
-        return id == pro.id;
-    }
-    @Override public int hashCode() {
-        return Objects.hash(id);
-    }
+    @Id @GeneratedValue @Include long    id;
+    @Column                      String  thumbs;
+    @Column                      byte[]  ref;
+    @Column(nullable = false)    boolean downloaded = true;
 }

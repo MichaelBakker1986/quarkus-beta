a79f85961d6590f8d9795619ea7ba12805771e18
MichaelBakker1986 <michael.bakker1986@gmail.com>
2021-03-22 02:48:12 +0100

    Invlude Xvideo Details 100x per day..

--- src/main/java/nl/appmodel/youporn/DailyYouPornRSSFeed.java
+++ src/main/java/nl/appmodel/youporn/DailyYouPornRSSFeed.java
@@ -1,23 +1,21 @@
 package nl.appmodel.youporn;
 
 import io.quarkus.scheduler.Scheduled;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import nl.appmodel.realtime.NodeJSProcess;
 import nl.appmodel.realtime.Notifier;
+import javax.enterprise.context.ApplicationScoped;
 import java.awt.TrayIcon.MessageType;
 @Slf4j
 @ToString
-@Getter
-@AllArgsConstructor
+@ApplicationScoped
 public class DailyYouPornRSSFeed {
     private static final String workspace = System.getenv("PROSITE_WORKSPACE");
     @SneakyThrows
     @Scheduled(cron = "0 30 23 * * ?", identity = "daily-youporn-rss-daily-crawl-deleted")
-    void DailyCrawlDeleted() {
+    public void DailyCrawlDeleted() {
         var status = new NodeJSProcess(workspace + "\\crawler\\youporn\\DailyCrawlYouporn.js").start();
         log.info("Process done" + status);
         new Notifier().displayTray("DailyCrawlYouporn - Finished", "DailyPornhubRSSFeed.DailyCrawlYouporn() done",
